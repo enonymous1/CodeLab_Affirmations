53ad7fc9330bbdc3483a99886480cafba597ba2f
Justin Lyons <jlyons2@student.fullsail.edu>
2022-06-26 23:44:44 -0400

    Test Lists and Adapters CodeLab

--- app/src/androidTest/java/com/sigmasavings/codelab_affirmations/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sigmasavings/codelab_affirmations/ExampleInstrumentedTest.kt
@@ -1,5 +1,11 @@
 package com.sigmasavings.codelab_affirmations
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +13,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +21,19 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class AffirmationsListTests {
+
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.sigmasavings.codelab_affirmations", appContext.packageName)
+    fun scroll_to_item() {
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(9)
+        )
+
+        onView(withText(R.string.affirmation10))
+            .check(matches(isDisplayed()))
     }
 }
--- app/src/test/java/com/sigmasavings/codelab_affirmations/ExampleUnitTest.kt
+++ app/src/test/java/com/sigmasavings/codelab_affirmations/ExampleUnitTest.kt
@@ -1,17 +1,28 @@
 package com.sigmasavings.codelab_affirmations
 
+import android.content.Context
+import com.sigmasavings.codelab_affirmations.adapter.ItemAdapter
+import com.sigmasavings.codelab_affirmations.model.Affirmation
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.mockito.Mockito.mock
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class AffirmationsAdapterTests {
+
+    private val context = mock(Context::class.java)
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
     }
 }
